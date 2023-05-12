24248593c7774ed66b74c539398455d4b91d08e9
heyrin1998 <qq5200@naver.com>
2023-05-12 15:02:28 +0900

    sitter

--- scripts/owner_detail.js
+++ scripts/owner_detail.js
@@ -1,14 +1,61 @@
 console.log('hi')
+let ownerId
 
-window.onload = async function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const ownerId = urlParams.get('owner_id');
+async function submitComment() {
+    const commentbox = document.getElementById("newcomment")
+    const newComment = commentbox.value
+    const response = await postComment(newComment, ownerId)
+    commentbox.value = ''
+    loadComments(ownerId)
 
+}
+
+async function loadComments(ownerID) {
+    const response = await getComments(ownerID)
+    console.log('코멘트로딩')
+    console.log(response)
+
+    const comment_list = document.getElementById("owner_comments")
+    // 로드할때 리셋
+    comment_list.innerHTML = ''
+    //댓글만들기
+    response.forEach(comment => {
+        comment_list.innerHTML += `
+        <li class="media my-4">
+            <div class="media-body">
+            <h5 class="mt-0 mb-1">${comment.writer}</h5>
+            <p class="margin-b">${comment.content}</p>
+            <p><small class="text-muted">${comment.created_at}</small></p>
+            </div>
+        </li>`
+    });
+}
+
+function owner_update(owner_id) {
+    window.location.href = `${frontend_base_url}/pet_owner_post.html?owner_id=${owner_id}`
+}
+
+async function loadOwner(ownerId) {
     const response = await getOwner(ownerId);
     console.log(response)
     // 글 제목
     const ownerTitle = document.getElementById("owner-title")
     ownerTitle.innerText = response.title
+
+    // 만약 글 작성자와 동일하다면 수정/삭제 버튼 내보내기
+    // const payload = localStorage.getItem("payload")
+    // const payload_parse = JSON.parse(payload)
+    // console.log(payload_parse.user_id)
+    const updateButton = document.createElement("button")
+    updateButton.setAttribute("class", "btn btn-dark")
+    updateButton.setAttribute("type", "button")
+    updateButton.setAttribute("onclick", `owner_update(${ownerId})`)
+    updateButton.innerHTML = "수정하기"
+    const buttons = document.getElementById("buttons")
+    buttons.appendChild(updateButton)
+    // 글 작성자
+    const ownerWriter = document.getElementById("owner-writer")
+    ownerWriter.innerText = response.writer
     // 예약상태
     const ownerIsReserved = document.getElementById("owner-isreserved")
     ownerIsReserved.innerText = response.is_reserved
@@ -18,6 +65,9 @@ window.onload = async function () {
     // 지역
     const ownerLocation = document.getElementById("owner-location")
     ownerLocation.innerText = response.location
+    // 반려동물 종
+    const ownerSpecies = document.getElementById("owner-species")
+    ownerSpecies.innerText = response.species
     // 요금
     const ownerPrice = document.getElementById("owner-price")
     ownerPrice.innerText = '₩' + response.charge
@@ -25,11 +75,21 @@ window.onload = async function () {
     const ownerContent = document.getElementById("owner-content")
     ownerContent.innerText = response.content
     // 게시글 사진
-    if (response.photo) {
+    if (response.photo != null) {
         const ownerImg = document.getElementById("owner-img")
         const newImg = document.createElement("img")
         newImg.setAttribute("src", `${backend_base_url}${response.photo}`)
         newImg.setAttribute("class", "img-fluid img-thumbnail")
         ownerImg.appendChild(newImg)
     }
+}
+
+window.onload = async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    ownerId = urlParams.get('owner_id');
+
+    await loadOwner(ownerId);
+    await loadComments(ownerId);
+
+
 }
--- scripts/sitter_list.js
+++ scripts/sitter_list.js
@@ -1,33 +1,33 @@
 console.log('연결')
 
-function owner_detail(owner_id) {
-    window.location.href = `${frontend_base_url}/pet_owner_detail.html?owner_id=${owner_id}`
+function sitter_detail(sitter_id) {
+    window.location.href = `${frontend_base_url}/pet_sitter_detail.html?sitter_id=${sitter_id}`
 }
 
-window.onload = async function loadOwners() {
-    owners = await getOwners()
-    console.log(owners)
+window.onload = async function loadSitters() {
+    sitters = await getSitters()
+    console.log(sitters)
 
-    const owner_list = document.getElementById("owner_list")
-    owners.forEach(owner => {
+    const sitter_list = document.getElementById("sitter_list")
+    sitters.forEach(sitter => {
         // 카드 큰 틀
         const newCard = document.createElement("div")
         newCard.setAttribute("class", "card mb-4")
-        newCard.setAttribute("id", owner.id)
+        newCard.setAttribute("id", sitter.id)
         newCard.setAttribute("style", "max-width: 90%;")
-        newCard.setAttribute("onclick", `owner_detail(${owner.id})`)
+        newCard.setAttribute("onclick", `sitter_detail(${sitter.id})`)
         const newRow = document.createElement("div")
         newRow.setAttribute("class", "row g-0")
         newCard.appendChild(newRow)
         // 카드 이미지
-        if (owner.photo) {
+        if (sitter.photo) {
             const newImg = document.createElement("div")
             newImg.setAttribute("class", "col-md-4")
             newRow.appendChild(newImg)
-            const ownerImg = document.createElement("img")
-            ownerImg.setAttribute("src", `${backend_base_url}${owner.photo}`)
-            ownerImg.setAttribute("class", "cardimg rounded-start")
-            newImg.appendChild(ownerImg)
+            const sitterImg = document.createElement("img")
+            sitterImg.setAttribute("src", `${backend_base_url}${sitter.photo}`)
+            sitterImg.setAttribute("class", "cardimg rounded-start")
+            newImg.appendChild(sitterImg)
         }
         // 사진이 없을 때 기본이미지를 넣을까요? 없앨까요?
 
@@ -44,18 +44,18 @@ window.onload = async function loadOwners() {
         // 구인 상태
         const newIsReserved = document.createElement("p")
         newIsReserved.setAttribute("class", "card-text")
-        newIsReserved.innerText = owner.is_reserved
+        newIsReserved.innerText = sitter.is_reserved
         newCardBody.appendChild(newIsReserved)
         // 게시글 제목
         const newTitle = document.createElement("h5")
         newTitle.setAttribute("class", "card-title")
-        newTitle.innerText = owner.title
+        newTitle.innerText = sitter.title
         newCardBody.appendChild(newTitle)
         // 게시글 내용
         const newContent = document.createElement("p")
         newContent.setAttribute("class", "card-text margin-b")
         newContent.setAttribute("id", "content")
-        newContent.innerText = owner.content
+        newContent.innerText = sitter.content
         newCardBody.appendChild(newContent)
         // 예약 날짜
         const newDateTime = document.createElement("p")
@@ -63,7 +63,7 @@ window.onload = async function loadOwners() {
         newCardBody.appendChild(newDateTime)
         const newReserved = document.createElement("small")
         newReserved.setAttribute("class", "text-muted")
-        newReserved.innerText = owner.reservation_start + ' - ' + owner.reservation_end
+        newReserved.innerText = sitter.reservation_start + ' - ' + sitter.reservation_end
         newDateTime.appendChild(newReserved)
         // 위치
         const newLocation = document.createElement("p")
@@ -71,7 +71,7 @@ window.onload = async function loadOwners() {
         newCardBody.appendChild(newLocation)
         const newPlace = document.createElement("small")
         newPlace.setAttribute("class", "text-muted")
-        newPlace.innerText = owner.location
+        newPlace.innerText = sitter.location
         newLocation.appendChild(newPlace)
         // 반려동물 + 요금
         const newFoot = document.createElement("div")
@@ -83,17 +83,17 @@ window.onload = async function loadOwners() {
         newFoot.appendChild(newSpecies)
         const Species = document.createElement("small")
         Species.setAttribute("class", "text-muted")
-        Species.innerText = owner.species
+        Species.innerText = sitter.species
         newSpecies.appendChild(Species)
         // 가격
         const newPrice = document.createElement("p")
         newPrice.setAttribute("class", "card-text col text-end fs-5 fw-semibold")
-        newPrice.innerText = '₩' + owner.charge
+        newPrice.innerText = '₩' + sitter.charge
         newFoot.appendChild(newPrice)
 
 
 
 
-        owner_list.appendChild(newCard)
+        sitter_list.appendChild(newCard)
     });
 }
